f1962ea2bc3af337a3bd72d442469c4747b008e7
Gavin Robertson <gavinrobertson@Joshs-MacBook-Pro-2.local>
2017-02-18 20:10:26 -0700

    Starting to allow events to be created

---
+++ Tag/Event.swift
@@ -0,0 +1,84 @@
+//
+//  Event.swift
+//  Tag
+//
+//  Created by Gavin Robertson on 2/18/17.
+//  Copyright © 2017 Tag Along. All rights reserved.
+//
+
+import Foundation
+import FirebaseAuth
+import FirebaseDatabase
+
+struct Event {
+    let itemRef:FIRDatabaseReference?
+    let key:String
+    let eventName:String
+    let owner:String
+    let location:String
+    let eventSummary:String
+    let privateEvent:Bool
+    let eventPicture:String
+    let taggedUsers: [String]
+    
+    init (snapshot:FIRDataSnapshot) {
+        
+        key = snapshot.key
+        itemRef = snapshot.ref
+        
+        let snapshotValue = snapshot.value as? NSDictionary
+        if let eventName = snapshotValue!["eventName"] as? String{
+            self.eventName = eventName
+        }else {
+            self.eventName = ""
+        }
+        
+        if let owner = snapshotValue!["owner"] as? String{
+            self.owner = owner
+        }else {
+            self.owner = ""
+        }
+        
+        if let eventSummary = snapshotValue!["eventSummary"] as? String{
+            self.eventSummary = eventSummary
+        }else {
+            self.eventSummary = ""
+        }
+        
+        if let location = snapshotValue!["location"] as? String{
+            self.location = location
+        }else {
+            self.location = ""
+        }
+        
+        if let privateEvent = snapshotValue!["privateEvent"] as? Bool{
+            self.privateEvent = privateEvent
+        }else {
+            self.privateEvent = false
+        }
+        
+        if let eventPicture = snapshotValue!["eventPicture"] as? String{
+            self.eventPicture = eventPicture
+        }else {
+            self.eventPicture = ""
+        }
+        self.taggedUsers = []
+    }
+    init (eventName:String, owner:String, eventSummary:String, location:String, privateEvent:Bool, eventPicture:String, key:String) {
+        self.eventName = eventName
+        self.owner = owner
+        self.eventSummary = eventSummary
+        self.location = location
+        self.privateEvent = privateEvent
+        self.eventPicture = eventPicture
+        self.key = ""
+        self.taggedUsers = []
+        self.itemRef = nil
+    }
+    func toAnyObject() -> NSDictionary {
+        
+        return ["eventName":eventName, "owner":owner, "eventSummary":eventSummary, "location":location, "privateEvent":privateEvent, eventPicture:"eventPicture", key:"key"]
+        
+    }
+    
+}
--- Tag/MyEventsView.swift
+++ Tag/MyEventsView.swift
@@ -7,89 +7,116 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class MyEventsView: UITableViewController {
-
+    
+    var dbRefEvent:FIRDatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        dbRefEvent = FIRDatabase.database().reference().child("events")
+        
     }
-
+    @IBAction func addEvent(_ sender: Any) {
+        let eventAlert = UIAlertController(title: "New Event", message: "Create Your Event", preferredStyle: .alert)
+        eventAlert.addTextField { (textField:UITextField) in
+            textField.placeholder = "Event Name"
+        }
+        eventAlert.addTextField { (textField:UITextField) in
+            textField.placeholder = "Event Details"
+        }
+        eventAlert.addTextField { (textField:UITextField) in
+            textField.placeholder = "Location"
+        }
+        eventAlert.addTextField { (textField:UITextField) in
+            textField.placeholder = "Location"
+        }
+        eventAlert.addTextField { (textField:UITextField) in
+            textField.placeholder = "Private Event"
+        }
+        eventAlert.addTextField { (textField:UITextField) in
+            textField.placeholder = "Add Picture"
+        }
+        eventAlert.addAction(UIAlertAction(title: "Send", style: .default, handler: { (action:UIAlertAction) in
+            let eventName = eventAlert.textFields?[0]
+            let eventDetails = eventAlert.textFields?[1]
+            let location = eventAlert.textFields?[2]
+        
+        }))
+        self.present(eventAlert, animated: true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 0
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return 0
     }
-
+    
     /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
+     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+     
+     // Configure the cell...
+     
+     return cell
+     }
+     */
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
